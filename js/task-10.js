8ddd5da1fd04cbfd5d6b44f5d92e1d53cf2a7428
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2023-10-18 20:55:10 +0300

    change style for gallery

--- js/task-10.js
+++ js/task-10.js
@@ -4,11 +4,6 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 const controlsDiv = document.querySelector('#controls');
 const fields = controlsDiv.querySelector('input');
 const createBtn = controlsDiv.querySelector('button[data-create]');
