e5c9c79eee68fff576a8bfdb769bf5518fa9ad82
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2023-10-18 17:10:11 +0300

    task8-js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const formObject = {};
+
+    formData.forEach((value, key) => {
+        formObject[key] = value;
+    });
+    if (!formObject.email || !formObject.password) {
+        alert("Всі поля повинні бути заповненні");
+    } else {
+        console.log(formObject);
+    }
+    loginForm.reset();
+});
